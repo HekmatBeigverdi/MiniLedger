23a6c367d9f88f34c89a0322459e6a57b7f44d17
hekmatbeigverdi <hb8info@gmail.com>
2026-04-17 19:11:01 +0330

    test: add unit tests for get accounts query handler

---
+++ tests/MiniLedger.Tests/Unit/Accounts/GetAccountsQueryHandlerTests.cs
@@ -0,0 +1,94 @@
+using FluentAssertions;
+using MiniLedger.Api.Features.Accounts.Queries.GetAccounts;
+using MiniLedger.Domain.Entities;
+using MiniLedger.Domain.Enums;
+using MiniLedger.Tests.Helpers;
+
+namespace MiniLedger.Tests.Unit.Accounts;
+
+public class GetAccountsQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_Should_Return_All_Accounts_When_No_Filter_Is_Applied()
+    {
+        // Arrange
+        var context = TestDbContextFactory.Create();
+
+        context.Accounts.AddRange(
+            new Account { Code = "1001", Name = "Cash", AccountType = AccountType.Asset, RequiresParty = false },
+            new Account { Code = "2001", Name = "Payable", AccountType = AccountType.Liability, RequiresParty = true }
+        );
+
+        await context.SaveChangesAsync();
+
+        var handler = new GetAccountsQueryHandler(context);
+        var query = new GetAccountsQuery();
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Data.Should().HaveCount(2);
+        result.TotalCount.Should().Be(2);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Filter_By_Search()
+    {
+        // Arrange
+        var context = TestDbContextFactory.Create();
+
+        context.Accounts.AddRange(
+            new Account { Code = "1001", Name = "Cash", AccountType = AccountType.Asset, RequiresParty = false },
+            new Account { Code = "1002", Name = "Bank", AccountType = AccountType.Asset, RequiresParty = false }
+        );
+
+        await context.SaveChangesAsync();
+
+        var handler = new GetAccountsQueryHandler(context);
+        var query = new GetAccountsQuery { Search = "Cash" };
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Data.Should().HaveCount(1);
+        result.Data.First().Name.Should().Be("Cash");
+    }
+
+    [Fact]
+    public async Task Handle_Should_Apply_Pagination()
+    {
+        // Arrange
+        var context = TestDbContextFactory.Create();
+
+        for (int i = 1; i <= 15; i++)
+        {
+            context.Accounts.Add(new Account
+            {
+                Code = $"10{i:D2}",
+                Name = $"Account {i}",
+                AccountType = AccountType.Asset,
+                RequiresParty = false
+            });
+        }
+
+        await context.SaveChangesAsync();
+
+        var handler = new GetAccountsQueryHandler(context);
+        var query = new GetAccountsQuery
+        {
+            PageNumber = 2,
+            PageSize = 5
+        };
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Data.Should().HaveCount(5);
+        result.PageNumber.Should().Be(2);
+        result.PageSize.Should().Be(5);
+        result.TotalCount.Should().Be(15);
+    }
+}
